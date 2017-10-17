05fae894994b523d21df0042f5776bc5d2316441
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2017-10-17 12:55:26 +1000

    Added nicer logging messages

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,35 +1,40 @@
-import { createReadStream } from 'fs'
+import { createReadStream, ReadStream, WriteStream } from 'fs'
 import { LargeObjectManager } from 'pg-large-object'
 
-export const uploadStream = async (db, inputStream) => {
-  return db.tx(async (tx: any) => {
+export const uploadStream = async (pgpDb, inputStream: ReadStream) => {
+  return pgpDb.tx(async (tx: any) => {
     const man = new LargeObjectManager({ pgPromise: tx })
 
     return man
       .createAndWritableStreamAsync()
-      .then(([oid, outputStream]: any) => {
+      .then(([oid, outputStream]: [number, WriteStream]) => {
+        console.info(`Created writable stream for OID:${oid}`)
         inputStream.pipe(outputStream)
 
         return new Promise((resolve, reject) => {
           let outputFinished = false
 
           inputStream.on('error', error => {
-            console.error('input stream error:', error)
+            console.error(`Input stream error for OID:${oid}`, error)
             reject(error)
           })
 
           inputStream.on('close', () => {
             if (!outputFinished) {
-              reject('input closed before output finished')
+              console.info(
+                `Input stream closed before output finished for OID:${oid}`
+              )
+              outputStream.end()
             }
           })
 
           outputStream.on('error', error => {
-            console.error('output stream error:', error)
+            console.error(`Output stream error for OID:${oid}`, error)
             reject(error)
           })
 
           outputStream.on('finish', () => {
+            console.info(`Finished writable stream for OID:${oid}`)
             outputFinished = true
             resolve(oid)
           })
@@ -38,6 +43,6 @@ export const uploadStream = async (db, inputStream) => {
   })
 }
 
-export const uploadFile = async (db, path: string) => {
-  return await uploadStream(db, createReadStream(path))
+export const uploadFile = async (pgpDb, path: string) => {
+  return await uploadStream(pgpDb, createReadStream(path))
 }
