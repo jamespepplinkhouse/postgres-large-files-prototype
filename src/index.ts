e64f23ba5845faaae01d573687e5f9eb392b3ced
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2017-11-02 18:19:12 +1000

    Refactoring download

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Example tests:
 // Download many files while uploading many files
+// Download files and kill the writeable stream
 // Upload files where we kill the stream randomly or throw errors
 
 import uploader from './uploader'
--- src/lib/download.ts
+++ src/lib/download.ts
@@ -1,77 +1,77 @@
 import { db } from './db'
 const { LargeObjectManager } = require('pg-large-object')
 
-type Callback = () => any
+// type Callback = () => any
 
-export default class LargeFileStreamer {
-  private manager
-  private state
+// export default class LargeFileStreamer {
+//   private manager
+//   private state
 
-  private forceCloseResolvers: Callback[] = []
-  private streamPromises: Array<Promise<any>> = []
+//   private forceCloseResolvers: Callback[] = []
+//   private streamPromises: Array<Promise<any>> = []
 
-  private closeTransactionFlag
+//   private closeTransactionFlag
 
-  constructor() {
-    this.state = 'uninitialised'
-  }
+//   constructor() {
+//     this.state = 'uninitialised'
+//   }
 
-  public init(): Promise<void> {
-    /* tslint:disable */
-    const that = this
-    /* tslint:enable */
-    return new Promise<void>((setupResolve, setupReject) => {
-      db.tx((transaction): Promise<any> => {
-        that.manager = new LargeObjectManager({ pgPromise: transaction })
-        that.state = 'open'
-        setupResolve()
+//   public init(): Promise<void> {
+//     /* tslint:disable */
+//     const that = this
+//     /* tslint:enable */
+//     return new Promise<void>((setupResolve, setupReject) => {
+//       db.tx((transaction): Promise<any> => {
+//         that.manager = new LargeObjectManager({ pgPromise: transaction })
+//         that.state = 'open'
+//         setupResolve()
 
-        return new Promise((resolve, reject) => {
-          // Wait until the close function is called
-          that.closeTransactionFlag = resolve
-        }).then(() => {
-          // Wait until all the stream have been closed
-          return Promise.all(that.streamPromises)
-        })
-      })
-    })
-  }
+//         return new Promise((resolve, reject) => {
+//           // Wait until the close function is called
+//           that.closeTransactionFlag = resolve
+//         }).then(() => {
+//           // Wait until all the stream have been closed
+//           return Promise.all(that.streamPromises)
+//         })
+//       })
+//     })
+//   }
 
-  public getStream(
-    id: number,
-    callback: (stream: NodeJS.ReadableStream, size: number) => any,
-    offset: number = 0
-  ): void {
-    if (this.state !== 'open') {
-      throw new Error('LargeFileStreamer is closed or not initialised')
-    }
-    /* tslint:disable */
-    const that = this
-    /* tslint:enable */
-    const streamPromise = that.manager
-      .openAndReadableStreamAsync(id)
-      .then(([size, stream]) => {
-        console.info('Streaming a large object with a total size of', size)
+//   public getStream(
+//     id: number,
+//     callback: (stream: NodeJS.ReadableStream, size: number) => any,
+//     offset: number = 0
+//   ): void {
+//     if (this.state !== 'open') {
+//       throw new Error('LargeFileStreamer is closed or not initialised')
+//     }
+//     /* tslint:disable */
+//     const that = this
+//     /* tslint:enable */
+//     const streamPromise = that.manager
+//       .openAndReadableStreamAsync(id)
+//       .then(([size, stream]) => {
+//         console.info('Streaming a large object with a total size of', size)
 
-        return new Promise((resolve, reject) => {
-          stream.on('end', resolve)
-          stream.on('error', reject)
+//         return new Promise((resolve, reject) => {
+//           stream.on('end', resolve)
+//           stream.on('error', reject)
 
-          that.forceCloseResolvers.push(() => {
-            stream.unpipe()
-            resolve()
-          })
+//           that.forceCloseResolvers.push(() => {
+//             stream.unpipe()
+//             resolve()
+//           })
 
-          callback(stream, size)
-        })
-      })
+//           callback(stream, size)
+//         })
+//       })
 
-    this.streamPromises.push(streamPromise)
-  }
+//     this.streamPromises.push(streamPromise)
+//   }
 
-  public close(): void {
-    this.state = 'closed'
-    this.forceCloseResolvers.map(callback => callback())
-    this.closeTransactionFlag()
-  }
-}
+//   public close(): void {
+//     this.state = 'closed'
+//     this.forceCloseResolvers.map(callback => callback())
+//     this.closeTransactionFlag()
+//   }
+// }
