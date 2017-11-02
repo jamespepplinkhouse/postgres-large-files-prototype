3a8d17a33eb8fe670cc5f5f68a0d01808ffe9364
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2017-11-02 15:43:12 +1000

    Downloader

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@
 // Upload files where we kill the stream randomly or throw errors
 
 import uploader from './uploader'
+import downloader from './downloader'
 
-uploader()
+// uploader()
+downloader()
