4b8c9db3731e80d951cdad1b3ad24032ec6962d3
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2017-11-07 12:37:57 +1000

    Added random offset; multiple oids; random errors

--- src/downloader.ts
+++ src/downloader.ts
@@ -3,24 +3,42 @@ import { v4 as uuid } from 'uuid'
 import { createWriteStream } from 'fs'
 import { db } from './lib/db'
 
+const oids = [16385, 16386, 16387, 16388, 16389]
+
 const downloadFile = async () => {
-  const oid = 16385
+  const oid = oids[Math.floor(Math.random() * oids.length)]
+
   console.info(`Downloading OID: ${oid}`)
 
   const outputFileName = `temp/${uuid()}.tmp`
   const outputFile = createWriteStream(outputFileName)
   console.info(`Output file name: ${outputFileName}`)
 
-  const { stream, size } = await download(db, oid)
+  const offset = Math.floor(Math.random() * 2000000)
+  console.info(`Offset: ${offset}`)
+  const { stream, size } = await download(db, oid, offset)
   console.info(`Got stream with size ${size}`)
 
   stream.pipe(outputFile)
-  outputFile.on('close', () => stream.destroy())
+
+  outputFile.on('error', err => {
+    console.log('### error ###: stream destroyed', err)
+    stream.destroy()
+  })
+
+  outputFile.on('close', () => {
+    console.log('### close ###: stream destroyed')
+    stream.destroy()
+  })
 
   // Kill the output stream
   setTimeout(() => {
-    console.log('Closing output stream')
-    outputFile.close()
+    const random = Math.random()
+    if (random > 0.66) {
+      outputFile.close()
+    } else if (random > 0.33) {
+      outputFile.emit('error', new Error('Oh no!!'))
+    }
   }, 50)
 }
 
